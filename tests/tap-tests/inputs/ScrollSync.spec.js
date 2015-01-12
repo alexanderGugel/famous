a0c94e9bec0ce9d00baf0e990e02155838ae5fc6
Alexander Gugel <alexander.gugel@gmail.com>
2015-01-12 11:15:00 +0100

    Test ScrollSync

--- tests/tap-tests/inputs/ScrollSync.spec.js
+++ tests/tap-tests/inputs/ScrollSync.spec.js
@@ -1,3 +1,6 @@
+// 60% DONE
+// TODO event format
+
 var test = require('tape');
 var Time = require('../../helpers/Time');
 Time.set(0);
@@ -11,15 +14,65 @@ test('ScrollSync', function(t) {
 
     t.test('start event', function(t) {
         t.plan(1);
-
-
+        Time.set(0);
+        var scrollSync = new ScrollSync();
+        scrollSync.on('start', function() {
+            t.pass('scrollSync should emit start event');
+        });
+        scrollSync._eventInput.emit('mousewheel', {
+            preventDefault: function() {},
+            clientX: 50,
+            clientY: 100,
+            offsetX: 3,
+            offsetY: 100
+        });
     });
     
-    // t.test('update event', function(t) {
-    //     t.plan(1);
-    // });
+    t.test('update event', function(t) {
+        t.plan(2);
+        Time.set(0);
+        var scrollSync = new ScrollSync();
+        scrollSync.on('update', function() {
+            t.pass('scrollSync should emit update event');
+        });
+        scrollSync._eventInput.emit('mousewheel', {
+            preventDefault: function() {},
+            clientX: 50,
+            clientY: 100,
+            offsetX: 3,
+            offsetY: 100
+        });
+        scrollSync._eventInput.emit('mousewheel', {
+            preventDefault: function() {},
+            clientX: 50,
+            clientY: 100,
+            offsetX: 3,
+            offsetY: 130
+        });
+    });
 
-    // t.test('end event', function(t) {
-    //     t.plan(1);
-    // });
+    t.test('end event', function(t) {
+        t.plan(1);
+        Time.set(0);
+        var scrollSync = new ScrollSync();
+        scrollSync.on('end', function() {
+            t.pass('scrollSync should emit end event');
+        });
+        scrollSync._eventInput.emit('mousewheel', {
+            preventDefault: function() {},
+            clientX: 50,
+            clientY: 100,
+            offsetX: 3,
+            offsetY: 100
+        });
+        Time.set(1);
+        scrollSync._eventInput.emit('mousewheel', {
+            preventDefault: function() {},
+            clientX: 50,
+            clientY: 100,
+            offsetX: 3,
+            offsetY: 130
+        });
+        Time.set(500);
+    });
 });
